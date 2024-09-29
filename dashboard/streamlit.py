5b138824ae326e370055585701be7e300299209b
egbertjandra <egbertjandra@gmail.com>
2024-09-30 03:12:12 +0700

    Update streamlit.py

--- dashboard/streamlit.py
+++ dashboard/streamlit.py
@@ -14,29 +14,30 @@ import seaborn as sns
 current_directory = os.path.dirname(__file__)
 csv_file_path = os.path.join(current_directory, 'bike_hour.csv')
 
-#Import data
+# Import data
 bike_hour = pd.read_csv(csv_file_path)
 
-#Judul dashboard
+# Judul dashboard
 st.title("Bike Sharing Data Analysis")
-#Sidebar
+
+# Sidebar
 st.sidebar.title("Bike Sharing Dashboard")
 st.sidebar.text("Use this dashboard to explore bike sharing data.")
 year = st.sidebar.slider("Select Year:", min_value=2011, max_value=2012, value=2011)
 
-#Menunjukan data
+# Menunjukan data
 with st.container():
     st.write("### Bike Sharing Data Preview:")
     st.write(bike_hour.head())
 
-#Membuat visualisasi dalam bentuk kolom
+# Membuat visualisasi dalam bentuk kolom
 col1, col2 = st.columns(2)
 
 with col1:
     st.write("### Average Bike Rentals by Season:")
     plt.figure(figsize=(10, 5))
     average_season = bike_hour.groupby('season')['cnt'].mean().reset_index()
-    sns.barplot(data=average_season, x='season', y='count', palette='viridis')
+    sns.barplot(data=average_season, x='season', y='cnt', palette='viridis')  # Perbaiki 'count' menjadi 'cnt'
     plt.title("Average Bike Rentals by Season")
     plt.xlabel("Season")
     plt.ylabel("Average Rentals")
@@ -46,16 +47,16 @@ with col2:
     st.write("### Average Bike Rentals by Season and Year:")
     plt.figure(figsize=(10, 5))
     average_season_year = bike_hour.groupby(['season', 'yr'])['cnt'].mean().reset_index()
-    sns.barplot(data=average_season_year, x='season', y='count', hue='yr', palette='magma')
+    sns.barplot(data=average_season_year, x='season', y='cnt', hue='yr', palette='magma')  # Perbaiki 'count' menjadi 'cnt'
     plt.title("Average Bike Rentals by Season and Year")
     plt.xlabel("Season")
     plt.ylabel("Average Rentals")
     st.pyplot(plt)
 
-#Memfilter data
+# Memfilter data
 filter_data = bike_hour[(bike_hour['yr'] == (year - 2011))]
 
-#Membuat Tab
+# Membuat Tab
 tab1, tab2 = st.tabs(["Monthly Rentals", "Yearly Overview"])
 
 with tab1:
@@ -77,7 +78,7 @@ with tab2:
     plt.ylabel("Total Rentals")
     st.pyplot(plt)
 
-#Expander
+# Expander
 with st.expander("Tentang Data dan Insight Visualisasi"):
     st.write(
         """Dataset ini berisi informasi tentang penyewaan sepeda, 
@@ -89,3 +90,4 @@ with st.expander("Tentang Data dan Insight Visualisasi"):
         Insight ini dapat membantu dalam perencanaan pengelolaan sepeda 
         sewa."""
     )
+
